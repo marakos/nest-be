77eebd669b8d617713545511a4a8dfbc99e5985d
Pete <petemarakos@gmail.com>
2022-07-26 23:38:02 +0300

    port test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,28 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('hello')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @HttpCode(200)
-  getHello(): { message: string } {
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/health')
+  @HttpCode(200)
+  getHealth(): string {
     return this.appService.getHello();
   }
 }
+
+// @GetMapping("/health")
+// public String healthCheck() throws IOException {
+//     return "Looks OK!";
+// }
+// @GetMapping("/")
+// public String health() throws IOException {
+//     return "Looks OK!";
+// }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { message: string } {
-    return { message: 'Hello World!2s1' };
+  getHello(): string {
+    return 'Looks OK!';
   }
 }
